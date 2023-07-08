e9d12eb96dd56f80939e290425f2cc931730de06
Your Name <karkibishal00@gmail.com>
2023-07-08 12:17:21 +1000

    planing, designing, of food ordering app, config driven ui

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="img">
+        <img src="" alt="logo" />
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
