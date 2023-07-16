b998017c01f76caf521bcc279a54bfaf17f91bd1
Your Name <karkibishal00@gmail.com>
2023-07-16 15:12:01 +1000

    feat: Implement search functionality in RestaurantMenu component after rendering the menu list of all the different restaurants in comp

--- src/pages/restaurant-menu/RestaurantMenu.js
+++ src/pages/restaurant-menu/RestaurantMenu.js
@@ -10,23 +10,62 @@ const RestaurantMenu = () => {
   const resMenu = useRestaurantMenu(resId);
   const [filteredResList, setFilteredResList] = useState([]);
   const [searchInput, setSearchInput] = useState("");
-  const [showAllRestaurants, setShowAllRestaurants] = useState(false);
 
-  console.log(resMenu);
-
-  const handleFilterClick = () => {};
-
-  const handleSeeAllRestaurants = () => {
-    setShowAllRestaurants(true);
+  const handleFilterClick = (filterName) => {
+    if (filterName === "all") {
+      setFilteredResList(
+        resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+          ?.card?.itemCards
+      );
+    } else {
+      const filteredItems =
+        resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards.filter(
+          (item) => item.card.info.type.toLowerCase() === filterName
+        );
+      setFilteredResList(filteredItems);
+    }
   };
+
+  const handleSearchClick = () => {
+    const filteredItems =
+      resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards.filter(
+        (item) =>
+          item.card.info.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    setFilteredResList(filteredItems);
+  };
+
+  useEffect(() => {
+    // Set the initial state of filteredResList to itemCards
+    const itemCards =
+      resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+        ?.card?.itemCards;
+    setFilteredResList(itemCards);
+  }, [resMenu]);
+
+  console.log(filteredResList);
 
   if (resMenu === null) return <ShimmerUI />;
 
   const { name, cuisines, costForTwoMessage } =
     resMenu?.cards[0]?.card?.card?.info;
 
-  const { itemCards } =
-    resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  // const { itemCards } =
+  //   resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  // console.log(itemCards);
+
+  // useState(() => {
+  //   setFilteredResList(itemCards);
+  // }, [itemCards]);
+
+  // useEffect(() => {
+  //   // Filter the menu items based on the search input
+  //   const filteredItems = itemCards.filter((item) =>
+  //     item.card.info.name.toLowerCase().includes(searchInput.toLowerCase())
+  //   );
+  //   setFilteredResList(filteredItems);
+  // }, [searchInput, resMenu]);
 
   return (
     <div className="restro-menu">
@@ -54,19 +93,7 @@ const RestaurantMenu = () => {
                 placeholder="Search by name or recipe"
                 title="Type in a name"
               />
-              <i
-                className="fas fa-search"
-                onClick={() => {
-                  // once got the search text in state
-                  // filter the restaurant list, render the comp and update the ui
-                  const filteredSearchRes = itemCards.filter((res) =>
-                    res.card.info.name
-                      .toLowerCase()
-                      .includes(searchInput.toLowerCase())
-                  );
-                  setFilteredResList(filteredSearchRes);
-                }}
-              ></i>
+              <i className="fas fa-search" onClick={handleSearchClick}></i>
             </div>
           </div>
           <div className="items">
@@ -115,50 +142,63 @@ const RestaurantMenu = () => {
             <span
               className="item"
               data-name="snacks"
-              nClick={() => handleFilterClick("snacks")}
+              onClick={() => handleFilterClick("snacks")}
             >
               Snacks
             </span>
           </div>
           <div className="menu-cards" id="menu-cards">
-            {itemCards.map((item) => (
-              <li id="meal" data-name="breakfast" key={item.card.info.id}>
-                <span>
-                  <div className="menu-card">
-                    <div className="meal-image">
-                      <img src={MEAL_IMG_URL + item.card.info.imageId} alt="" />
-                    </div>
-                    <div className="meal-content">
-                      <div className="meal-content-top">
-                        <div className="meal-title-content">
-                          <h2 className="meal-title">{item.card.info.name}</h2>
-                          <span className="meal-price">
-                            ${" "}
-                            {item.card.info.defaultPrice / 80 ||
-                              item.card.info.price / 80}
-                          </span>
+            {filteredResList && filteredResList.length > 0 ? (
+              filteredResList.map((item) => (
+                <li
+                  id="meal"
+                  data-name={item.card.info.type}
+                  key={item.card.info.id}
+                >
+                  <span>
+                    <div className="menu-card">
+                      <div className="meal-image">
+                        <img
+                          src={MEAL_IMG_URL + item.card.info.imageId}
+                          alt=""
+                        />
+                      </div>
+                      <div className="meal-content">
+                        <div className="meal-content-top">
+                          <div className="meal-title-content">
+                            <h2 className="meal-title">
+                              {item.card.info.name}
+                            </h2>
+                            <span className="meal-price">
+                              ${" "}
+                              {item.card.info.defaultPrice / 80 ||
+                                item.card.info.price / 80}
+                            </span>
+                          </div>
+                          <div className="meal-review">
+                            <div className="meal-star">
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star-half-alt"></i>
+                            </div>
+                            <div className="meal-review-text">
+                              <span>34 reviews</span>
+                            </div>
+                          </div>
                         </div>
-                        <div className="meal-review">
-                          <div className="meal-star">
-                            <i className="fas fa-star"></i>
-                            <i className="fas fa-star"></i>
-                            <i className="fas fa-star"></i>
-                            <i className="fas fa-star"></i>
-                            <i className="fas fa-star-half-alt"></i>
-                          </div>
-                          <div className="meal-review-text">
-                            <span>34 reviews</span>
-                          </div>
+                        <div className="meal-content-bottom">
+                          {item.card.info.description}
                         </div>
                       </div>
-                      <div className="meal-content-bottom">
-                        {item.card.info.description}
-                      </div>
                     </div>
-                  </div>
-                </span>
-              </li>
-            ))}
+                  </span>
+                </li>
+              ))
+            ) : (
+              <p>No items found</p>
+            )}
           </div>
         </div>
       </section>
